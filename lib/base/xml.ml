2fdcf2e15a91ad8044187d01bd09ab63e13374f6
Jay Xu <jay.xu.krfantasy@gmail.com>
2025-12-23 19:42:45 +0800

    feat(xml): Add exception type and pretty printers

--- lib/base/xml.ml
+++ lib/base/xml.ml
@@ -2,6 +2,7 @@ type t =
   | Element of { name: string; attrs: (string * string) list; childs: t list }
   | Data of string
 
+exception Invalid_Xml of t * string
 
 let get_name = function
   | Element { name; _ } -> name
@@ -82,3 +83,55 @@ let read_file filename =
 let read_string s =
   let i = Xmlm.make_input ~strip:true (`String (0, s)) in
   Xmlm.input_doc_tree ~el:Parser.make_element ~data:Parser.make_data i |> snd
+
+(* Escape XML special characters to entities *)
+let escape_xml s =
+  let len = String.length s in
+  let buf = Buffer.create (len * 2) in
+  String.iter (function
+    | '<' -> Buffer.add_string buf "&lt;"
+    | '>' -> Buffer.add_string buf "&gt;"
+    | '&' -> Buffer.add_string buf "&amp;"
+    | '"' -> Buffer.add_string buf "&quot;"
+    | '\'' -> Buffer.add_string buf "&apos;"
+    | c -> Buffer.add_char buf c
+  ) s;
+  Buffer.contents buf
+
+(* Compact pretty printer without indentation or newlines *)
+let rec pp fmt = function
+  | Element {name; attrs; childs} ->
+    Format.fprintf fmt "<%s" name;
+    List.iter (fun (k, v) ->
+      Format.fprintf fmt " %s=\"%s\"" k (escape_xml v)
+    ) attrs;
+    Format.fprintf fmt ">";
+
+    List.iter (fun child -> pp fmt child) childs;
+
+    Format.fprintf fmt "</%s>" name
+  | Data s ->
+    Format.fprintf fmt "%s" (escape_xml s)
+
+(* Pretty printer with indentation and newlines *)
+let rec pp_pretty fmt = function
+  | Element {name; attrs; childs} ->
+    (* @[<v 2> opens a vertical box with 2-space indentation *)
+    Format.fprintf fmt "@[<v 2><%s" name;
+    List.iter (fun (k, v) ->
+      Format.fprintf fmt " %s=\"%s\"" k (escape_xml v)
+    ) attrs;
+    Format.fprintf fmt ">";
+
+    (* @, adds a cut hint - newline if needed before each child *)
+    List.iter (fun child -> Format.fprintf fmt "@,%a" pp_pretty child) childs;
+
+    Format.fprintf fmt "@]</%s>" name
+  | Data s ->
+    if s <> "" then Format.fprintf fmt "%s" (escape_xml s)
+
+let to_string xml =
+  Fmt.str "%a" pp_pretty xml
+
+let pp_invalid_xml fmt (xml, msg) =
+  Fmt.pf fmt "%s@\nProblematic XML:\n%a@\n" msg pp_pretty xml
